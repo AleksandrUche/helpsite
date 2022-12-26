5cd6976043a7bc2ec27ab00a6174ac7cbc2c5420
AleksandrUche <zak.reizer@mail.ru>
2022-12-27 01:53:30 +0300

    изменены и добавлены модели для ГОСТ 33259, добавлены модели для АТК фланцев

--- gost/models.py
+++ gost/models.py
@@ -18,36 +18,105 @@ class CategoryDoc(models.Model):
         return self.name
 
 
-class Gost33259Flange(models.Model):
-    dn_passage = models.CharField(max_length=6)
-    pn = models.CharField(max_length=6)
-    d2 = models.CharField(blank=True, null=True, max_length=6)
-    d3 = models.CharField(blank=True, null=True, max_length=6)
-    d4 = models.CharField(blank=True, null=True, max_length=6)
-    d5 = models.CharField(blank=True, null=True, max_length=6)
-    d6 = models.CharField(blank=True, null=True, max_length=6)
-    d7 = models.CharField(blank=True, null=True, max_length=6)
-    d8 = models.CharField(blank=True, null=True, max_length=6)
-    d9 = models.CharField(blank=True, null=True, max_length=6)
-    d10 = models.CharField(blank=True, null=True, max_length=6)
-    d11 = models.CharField(blank=True, null=True, max_length=6)
-    b2_lower = models.CharField(blank=True, null=True, max_length=6)
-    h_lower = models.CharField(blank=True, null=True, max_length=6)
-    h1_lower = models.CharField(blank=True, null=True, max_length=6)
-    h2_lower = models.CharField(blank=True, null=True, max_length=6)
-    h3_lower = models.CharField(blank=True, null=True, max_length=6)
-    h4_lower = models.CharField(blank=True, null=True, max_length=6)
-    h5_lower = models.CharField(blank=True, null=True, max_length=6)
-    dm = models.CharField(blank=True, null=True, max_length=6)
-    dn = models.CharField(blank=True, null=True, max_length=6)
-    d1_lower = models.CharField(blank=True, null=True, max_length=6)
-    b_lower = models.CharField(blank=True, null=True, max_length=6)
-    h = models.CharField(blank=True, null=True, max_length=6)
-    d = models.CharField(blank=True, null=True, max_length=6)
-    d1 = models.CharField(blank=True, null=True, max_length=6)
-    d_lower = models.CharField(blank=True, null=True, max_length=6)
-    n_lower = models.CharField(blank=True, null=True, max_length=6)
-    pin = models.CharField(blank=True, null=True, max_length=6)
+'''
+Начало описания моделей для ГОСТ 33259-2015
+'''
+
+
+# наличие фланца в ГОСТ 33259-2015
+class Gost33259AvailabilityFlange(models.Model):
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    type_fl = models.CharField(max_length=6, verbose_name='Тип фланца')
+    dn_10 = models.BooleanField(default=False)
+    dn_15 = models.BooleanField(default=False)
+    dn_20 = models.BooleanField(default=False)
+    dn_25 = models.BooleanField(default=False)
+    dn_32 = models.BooleanField(default=False)
+    dn_40 = models.BooleanField(default=False)
+    dn_50 = models.BooleanField(default=False)
+    dn_65 = models.BooleanField(default=False)
+    dn_80 = models.BooleanField(default=False)
+    dn_100 = models.BooleanField(default=False)
+    dn_125 = models.BooleanField(default=False)
+    dn_150 = models.BooleanField(default=False)
+    dn_200 = models.BooleanField(default=False)
+    dn_250 = models.BooleanField(default=False)
+    dn_300 = models.BooleanField(default=False)
+    dn_350 = models.BooleanField(default=False)
+    dn_400 = models.BooleanField(default=False)
+    dn_450 = models.BooleanField(default=False)
+    dn_500 = models.BooleanField(default=False)
+    dn_600 = models.BooleanField(default=False)
+    dn_700 = models.BooleanField(default=False)
+    dn_800 = models.BooleanField(default=False)
+    dn_900 = models.BooleanField(default=False)
+    dn_1000 = models.BooleanField(default=False)
+    dn_1200 = models.BooleanField(default=False)
+    dn_1400 = models.BooleanField(default=False)
+    dn_1600 = models.BooleanField(default=False)
+    dn_1800 = models.BooleanField(default=False)
+    dn_2000 = models.BooleanField(default=False)
+    dn_2200 = models.BooleanField(default=False)
+    dn_2400 = models.BooleanField(default=False)
+    dn_2600 = models.BooleanField(default=False)
+    dn_2800 = models.BooleanField(default=False)
+    dn_3000 = models.BooleanField(default=False)
+    dn_3200 = models.BooleanField(default=False)
+    dn_3400 = models.BooleanField(default=False)
+    dn_3600 = models.BooleanField(default=False)
+    dn_4000 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.pn
+
+
+# Тип 01
+class Gost33259Type01(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    dv_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='dв')
+    b_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='b')
+    c1_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='c1')
+    d = models.CharField(blank=True, null=True, max_length=6, verbose_name='D')
+    d1 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D1')
+    d_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='d')
+    n_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='n')
+    pin = models.CharField(blank=True, null=True, max_length=6, verbose_name='Диаметр шпилек')
+
+
+# Тип 02
+class Gost33259Type02(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d0 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D0')
+    d2 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D2')
+    dv_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='dв')
+    b_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='b')
+    b1_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='b1')
+    c_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='c')
+    c1_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='c1')
+    d = models.CharField(blank=True, null=True, max_length=6, verbose_name='D')
+    d1 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D1')
+    d_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='d')
+    n = models.CharField(blank=True, null=True, max_length=6, verbose_name='n')
+    pin = models.CharField(blank=True, null=True, max_length=6, verbose_name='Диаметр шпилек')
+
+
+# Тип 11
+class Gost33259Type11(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    dm = models.CharField(blank=True, null=True, max_length=6, verbose_name='Dm')
+    dn = models.CharField(blank=True, null=True, max_length=6, verbose_name='Dn')
+    d1_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='d1')
+    b_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='b')
+    h = models.CharField(blank=True, null=True, max_length=6, verbose_name='H')
+    h1 = models.CharField(blank=True, null=True, max_length=6, verbose_name='H1')
+    d = models.CharField(blank=True, null=True, max_length=6, verbose_name='D')
+    d1 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D1')
+    d_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='d')
+    n_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='n')
+    pin = models.CharField(blank=True, null=True, max_length=6, verbose_name='Диаметр шпилек')
 
     def __str__(self):
         return self.dn_passage
@@ -58,7 +127,34 @@ class Gost33259Flange(models.Model):
         ordering = ['id']
 
 
-class Gost33259Type(models.Model):
+# уплотнительные поверхности для всех фланцев ГОСТ 33259
+class Gost33259SurfaceValues(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d2 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D2')
+    d3 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D3')
+    d4 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D4')
+    d5 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D5')
+    d6 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D6')
+    d7 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D7')
+    d8 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D8')
+    d9 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D9')
+    d10 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D10')
+    d11 = models.CharField(blank=True, null=True, max_length=6, verbose_name='D11')
+    b2_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='b2')
+    h_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h')
+    h1_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h1')
+    h2_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h2')
+    h3_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h3')
+    h4_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h4')
+    h5_lower = models.CharField(blank=True, null=True, max_length=6, verbose_name='h5')
+
+    def __str__(self):
+        return self.dn_passage
+
+
+# чертеж типа фланца
+class Gost33259TypeDrawing(models.Model):
     type_fl = models.CharField(max_length=6)
     flange_drawing = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
@@ -70,7 +166,8 @@ class Gost33259Type(models.Model):
         verbose_name_plural = 'Типы фланцев ГОСТ 33259-2015'
 
 
-class Gost33259Surface(models.Model):
+# чертеж уплотнительной поверхности фланца
+class Gost33259SurfaceDrawing(models.Model):
     surface_fl = models.CharField(max_length=6)
     flange_surface = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
@@ -81,3 +178,134 @@ class Gost33259Surface(models.Model):
         verbose_name = 'уплотнительные поверхности'
         verbose_name_plural = 'Уплотнительная поверхность ГОСТ 33259-2015'
 
+
+'''
+Конец описания моделей для ГОСТ 33259-2015
+'''
+
+'''
+Начало описания моделей для АТК 26-18-13-96
+'''
+
+
+# Исполнение 1
+class Atk261813FlangeExec1(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d2 = models.CharField(max_length=6, verbose_name='D2')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h1_lower = models.CharField(max_length=6, verbose_name='h1')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+# Исполнение 2
+class Atk261813FlangeExec2(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d4 = models.CharField(max_length=6, verbose_name='D4')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h2_lower = models.CharField(max_length=6, verbose_name='h2')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+# Исполнение 3
+class Atk261813FlangeExec3(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d2 = models.CharField(max_length=6, verbose_name='D2')
+    d6 = models.CharField(max_length=6, verbose_name='D6')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h1_lower = models.CharField(max_length=6, verbose_name='h1')
+    h2_lower = models.CharField(max_length=6, verbose_name='h2')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+# Исполнение 4
+class Atk261813FlangeExec4(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d2 = models.CharField(max_length=6, verbose_name='D2')
+    d4 = models.CharField(max_length=6, verbose_name='D4')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h2_lower = models.CharField(max_length=6, verbose_name='h2')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+# Исполнение 5
+class Atk261813FlangeExec5(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d2 = models.CharField(max_length=6, verbose_name='D2')
+    d5 = models.CharField(max_length=6, verbose_name='D5')
+    d6 = models.CharField(max_length=6, verbose_name='D6')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h1_lower = models.CharField(max_length=6, verbose_name='h1')
+    h2_lower = models.CharField(max_length=6, verbose_name='h2')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+# Исполнение 6
+class Atk261813FlangeExec6(models.Model):
+    dn_passage = models.CharField(max_length=6, verbose_name='DN')
+    pn = models.CharField(max_length=6, verbose_name='PN')
+    d = models.CharField(max_length=6, verbose_name='D')
+    d1 = models.CharField(max_length=6, verbose_name='D1')
+    d8 = models.CharField(max_length=6, verbose_name='D8')
+    d9 = models.CharField(max_length=6, verbose_name='D9')
+    d1_lower = models.CharField(max_length=6, verbose_name='d1')
+    b_lower = models.CharField(max_length=6, verbose_name='b')
+    h_lower = models.CharField(max_length=6, verbose_name='h')
+    h1_lower = models.CharField(max_length=6, verbose_name='h1')
+    h3_lower = models.CharField(max_length=6, verbose_name='h3')
+    b1_lower = models.CharField(max_length=6, verbose_name='b1')
+    dm = models.CharField(max_length=6, verbose_name='Dm')
+    dn = models.CharField(max_length=6, verbose_name='Dn')
+    r_lower = models.CharField(max_length=6, verbose_name='r')
+    d_lower = models.CharField(max_length=6, verbose_name='d')
+    n_lower = models.CharField(max_length=6, verbose_name='n')
+    m = models.CharField(max_length=6, verbose_name='Масса')
+
+
+'''
+Конец описания моделей для АТК 26-18-13-96
+'''
